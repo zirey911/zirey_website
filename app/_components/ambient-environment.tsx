5276161d9c2665ed756f290838b57a97ee0c0cc4
Zirey <ronshergill2010@gmail.com>
2026-09-21 12:17:33 +0400

    Debug ambient CSS

--- app/_components/ambient-environment.tsx
+++ app/_components/ambient-environment.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import type { AnimationEvent, PointerEvent } from "react";
-import "./ambient-environment.css";
+//import "./ambient-environment.css";
 
 type Season = "spring" | "summer" | "autumn" | "winter";
 type Mode = "day" | "irl" | "night";
